9c9afc325e5657e8cedd9efacc2f2bea174e6303
MatecoKoeln <sascha.wasel@mateco.de>
2026-08-07 20:08:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const herstellerDaten = {
 
     '📘 Serviceanleitung': {
       'Serviceanleitung SN GS32D-25101':
-        'pdf/Genie/GS1932/Serviceanleitung SN GS32T_25101.pdf',
+        'pdf/Genie/GS1932/Serviceanleitung SN GS32D_25101.pdf',
 
       'Serviceanleitung SN GS32T-10001':
         'pdf/Genie/GS1932/Serviceanleitung SN GS32T_10001.pdf'
